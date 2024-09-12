d2b17fa8c92e6bf7073f4a88f4847ed078b13e57
Souza <edirdizinho@gmail.com>
2024-09-12 18:21:09 -0300

    Add files via upload

---
+++ 01-aula01.py
@@ -0,0 +1,20 @@
+# Aqui eu posso adicionar comentários
+# Continuar na linha de baixo
+print("Edir") # comentário a linha do código
+print('Edir')
+print('Edir "Souza" Endryl')
+print("Edir 'Souza' Endryl")
+print("Edir \"Souza\" Endryl")
+
+teste= "oi"
+print(teste)
+print(type(teste))
+teste = 17
+print(teste)
+print(type(teste))
+teste = 12.1
+print(teste)
+print(type(teste))
+teste = False
+print (teste)
+print(type(teste))
---
+++ 02-aula02.py
@@ -0,0 +1,9 @@
+# atribuição das variáveis
+nome = "Edir"
+idade = 16
+altura = 1.79
+
+# exibir mensagem na tela
+print("nome: ", nome, " idade: ", idade, " altura: ", altura)
+# formação com f-string
+print(f"nome: {nome} idade: {idade} altura: {altura}")
